dfa4d43a195de36371db1cce335e50d218c167ff
mhtang <mhtang@thoughtworks.com>
2023-11-20 17:01:42 +0800

    Update MongoDB connection URI to include username and password

--- app.config.js
+++ app.config.js
@@ -28,10 +28,13 @@ exports.CROSS_DOMAIN = {
 };
 
 exports.MONGODB = {
-  uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/blogNode`,
+  uri: `mongodb://${argv.db_username || 'DB_username'}:${
+    argv.db_password || 'DB_password'
+  }@127.0.0.1:${argv.dbport || '27017'}/blogNode`,
   username: argv.db_username || 'DB_username',
   password: argv.db_password || 'DB_password',
 };
+
 exports.AUTH = {
   data: argv.auth_data || { user: 'root' },
   jwtTokenSecret: argv.auth_key || 'blog-node',
